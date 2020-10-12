8d8e9f912946706700725f7cf5e1af82ad92b26f
Tanumay Dhar <tanna.dhar@gmail.com>
2020-10-12 17:46:07 +0530

    Maven Hello world - email test

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("This is maven project");
+		//System.out.println("This is maven project");
+		
+		System.out.println("This is for email test");
 		// TODO Auto-generated method stub
 
 	}
